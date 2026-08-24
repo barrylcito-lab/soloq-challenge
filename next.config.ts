3d11e904593c1ec61db02be605148af114dfd30e
barrylcito-lab <barrylcito@gmail.com>
2026-08-24 15:17:20 -0400

    bypass typescript build errors

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   typescript: {
-    // Ignora los errores de tipos al compilar en Vercel
     ignoreBuildErrors: true,
   },
   eslint: {
-    // Ignora las advertencias de ESLint al compilar
     ignoreDuringBuilds: true,
   },
 };
